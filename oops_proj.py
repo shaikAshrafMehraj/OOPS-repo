f747d6eb540be487a1675a51fa351fb597a37530
sam <ashraf.ul.mehraj@gmail.com>
2025-12-29 17:27:51 +0530

    2 signIn is done

--- oops_proj.py
+++ oops_proj.py
@@ -7,21 +7,45 @@ class Chatbook:
     
     def menu(self):
         userInput = input("""Welcome to chatbook !
-                            press 1 to signup
-                            press 2 to signIn
-                            press 3 to write a post
-                            press 4 to message a friend
-                            press any other key to exit""")
+                            1. press 1 to signup
+                            2. press 2 to signIn
+                            3. press 3 to write a post
+                            4. press 4 to message a friend
+                            5. press any other key to exit""")
         if userInput == "1":
-            pass
+            self.signUp()
         elif userInput == "2":
-            pass
+            self.signIn()
         elif userInput == "3":
             pass
         elif userInput == "4":
             pass
         else :
-            exit
+            exit()
 
+    def signUp(self):
+        email = input("Enter the email here :")
+        pwd = input("Enter the pwd here :")
+        self.userName = email
+        self.passWord = pwd
+        print("You have signed successfully ..")
+        print("\n")
+        self.menu()
+
+    def signIn(self):
+        if self.userName == " " and self.passWord == " ":
+            print("Please signUP first then signIn later ")
+        else :
+            uname = input("Enter the email/ username here :")
+            pwd = input("Enter the email/ username here :")
+            if self.userName == uname and self.passWord == pwd:
+                print("you are signed In sucessfully !!")
+                self.loggedIn = True
+            else :
+                print("Please input correct credentials..")
+        print("\n")
+        self.menu()
+
+        
 obj = Chatbook()
 
